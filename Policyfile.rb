de075468fd7410c059b181f29015034ccff4517f
PRGU <prgu@example.com>
2020-09-26 22:30:33 +0200

    commit

--- Policyfile.rb
+++ Policyfile.rb
@@ -11,6 +11,7 @@ default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
 run_list 'git_cookbook::default'
+named_run_list :server, 'git_cookbook::server'
 
 # Specify a custom source for a single cookbook:
 cookbook 'git_cookbook', path: '.'
